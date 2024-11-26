68a07e4961fb8666d53b510a29b631a28b8e8eb7
Pedro <pedro.colaressrubens15789@gmail.com>
2024-11-26 10:19:23 -0400

    version 1.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./conta/**/*.{html,js}"],
   theme: {
     extend: {
       backgroundImage: {
